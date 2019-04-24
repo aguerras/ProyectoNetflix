05077f891e2643444868d481c788f19f700faacf
aguerras <aguerra2012201@gmail.com>
2019-04-23 22:46:51 -0600

    Agregar Views y Tittle.aspx

---
+++ ProyectoNetflix/Views/Title.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Title.aspx.cs" Inherits="ProyectoNetflix.Views.Title" %>
+<asp:Content ID="MainContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="video-page">
+        <div class="video-page-header">
+            <div class="video-description">
+                <div class="header-left">
+                    <asp:Image id="img_movie" runat="server" ImageUrl="Images/default.jpg"/>
+                    <div class="my-list">
+                        <i class="fas fa-check"></i> Mi lista
+                    </div>
+                </div>
+            <div class="header-right">
+                <h1><asp:Label id="lbl_title" Text="" runat="server" /></h1>
+                <asp:Label id="lbl_description" Text="" runat="server" />
+                <div class='info'>
+                    <h4>Categoria:</h4>
+                    <h5>
+                        <asp:Label id="lbl_category" Text="" runat="server" />
+                    </h5>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="streaming-container">
+            <img src="http://4.bp.blogspot.com/-Xx_xBL54Pas/TtkId59pLuI/AAAAAAAAIUc/T7v0-EqgEd0/s1600/bigfish5.jpg"/>
+            <div class="streaming-play">
+                <i class="fas fa-play"></i>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ ProyectoNetflix/Views/Title.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EstructuraDeDatos;
+using ProyectoNetflix.Model;
+
+namespace ProyectoNetflix.Views
+{
+    public partial class Title : System.Web.UI.Page
+    {
+        public LinkedListDouble movies = new LinkedListDouble();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    String name = Request.QueryString["name"];
+
+                    if (name.Length > 0)
+                    {
+                        movies = (LinkedListDouble)Session["movies"];
+                        movies.iniciarPrimero();
+                        while (movies.getActual() != null)
+                        {
+                            Movie movie;
+                            movie = (Movie)movies.getActual();
+                            lbl_category.Text = movie.Category;
+                            lbl_title.Text = movie.Name;
+                            lbl_description.Text = movie.Description;
+                            img_movie.ImageUrl = "../" + movie.Picture;
+                            String movieName = movie.Name;
+                            if (movieName == name)
+                            {
+                                return;
+                            }
+                            movies.next();
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+
+                    //throw;
+                }
+            }
+        }
+    }
+}
