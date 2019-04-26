de1e96822b750982cae689fd0e8d30c09006622b
aguerras <aguerra2012201@gmail.com>
2019-04-26 00:42:30 -0600

    Agregar la vista MyList

---
+++ ProyectoNetflix/Views/MyList.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MyList.aspx.cs" Inherits="ProyectoNetflix.Views.MyList" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="contain">
+        <h1>MI LISTA</h1>
+        <p>Todas las peliculas de mi Lista.</p>
+        <div class="row">
+            <div class="row__inner">
+
+                <%= peliculas %>
+                <div class="tile">
+                    <div class="tile__media">
+                        <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-1.jpg" alt=""  />
+                    </div>
+                    <div class="tile__details">
+                        <div class="tile__title">
+                            Top Gear
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ ProyectoNetflix/Views/MyList.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EstructuraDeDatos;
+using Newtonsoft.Json;
+using ProyectoNetflix.Model;
+
+namespace ProyectoNetflix.Views
+{
+    public partial class MyList : System.Web.UI.Page
+    {
+        public String peliculas;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Movie movie;
+                Cola myList = (Cola)Session["myList"];
+                Cola myListMovies = myList.Clone();
+                while (myListMovies.getPrimero() != null)
+                {
+                    movie = (Movie)myListMovies.getPrimero();
+                    String movieName = movie.Name;
+                    if (movieName.Length > 32)
+                    {
+                        movieName = movieName.Substring(0, 33) + "...";
+                    }
+                    peliculas += "<div class='tile'><a href='Title.aspx?name=" + movieName + "&category=" + movie.Category + "&boton=1'><div class='tile__media'><img class='tile__img' src='../" + movie.Picture + "' alt=''  /></div><div class='tile__details'><div class='tile__title'>" + movieName + "</div></div></a></div>";
+                    myListMovies.pop();
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+    }
+}
